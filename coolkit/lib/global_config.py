81a4bae85d5423d1c3074b33209210b10ea64dad
Sarbjit Singh <srbcheema1@gmail.com>
2018-10-06 00:51:37 +0530

    more smart global_config

--- coolkit/lib/global_config.py
+++ coolkit/lib/global_config.py
@@ -1,95 +1,34 @@
-import getpass
+import os
+import shutil
+from importlib.util import spec_from_file_location, module_from_spec
+
+from .abs_path import abs_path
+from .Constants import Const
+from .files import verify_file
+
+def create_global_config():
+    path_of_global_config = '/'.join(abs_path(__file__).split('/')[:-2])+'/extra/global_config.py'
+
+    print('not found global config, cwd is ', path_of_global_config)
+    shutil.copy(path_of_global_config, abs_path('~/.config/coolkit/global_config.py'))
+
+# load file
+def load_module(path):
+    path = abs_path(path)
+    if(not os.path.isfile(path)):
+        create_global_config()
+    mod_name = path.split('/')[-1].split('.')[0]
+
+    global global_config
+    spec = spec_from_file_location(mod_name,path)
+    global_config = module_from_spec(spec)
+    spec.loader.exec_module(global_config)
+
+load_module(Const.cache_dir+'/global_config.py')
 
 def get_contest_name(folder):
-    '''
-    takes parameter folder which is name of folder you are in
-    returns contest number if possible to detect
-    if can't determine then return None
-    '''
-    user = getpass.getuser()
-    if(user == 'srb'):
-        # I kept for me, you too can use this ready made function
-        return srb_contest_name(folder)
-    return None
+    return global_config.get_contest_name(folder)
+
 
 def get_problem_name(file_name):
-    '''
-    takes parameter file_name which is name of file you are working with
-    returns problem name if possible to detect
-    if can't determine then return None
-    '''
-    user = getpass.getuser()
-    if(user == 'srb'):
-        # I kept for me, you too can use this ready made function
-        return srb_problem_name(file_name) # my way
-    return None
-
-
-
-def srb_contest_name(folder):
-    '''
-    my way to get contest name from folder name
-    '''
-    newstr = ''.join((ch if ch in '0123456789' else ' ') for ch in folder)
-    number_list = [int(i) for i in newstr.split()]
-    if(len(number_list) == 1):
-        return str(number_list[0])
-    return None
-
-
-def srb_problem_name(file_name):
-    '''
-    my way to get problem name from file_name
-    '''
-    file_name = file_name.split('.')[0] #chopp off extension
-    file_name = file_name.lower()
-
-    # file_name = file_name.replace('_','')
-    file_name = file_name.replace('-','')
-    file_name = file_name.replace('good','')
-    file_name = file_name.replace('test','')
-    file_name = file_name.replace('wrong','')
-    file_name = file_name.replace('fine','')
-    file_name = file_name.replace('bad','')
-
-    mapp = {
-        'one':'A',
-        'two':'B',
-        'three':'C',
-        'four':'D',
-        'five':'E',
-        'six':'F',
-        'seven':'G',
-        'eight':'H',
-        'nine':'I',
-
-    }
-
-
-    for key in mapp.keys():
-        if(key in file_name):
-            return mapp[key]
-
-    if(len(file_name) == 1 and file_name.isalpha()):
-        return file_name.upper()
-
-    name_arr = file_name.split('_')
-    for part in name_arr:
-        if(len(part) == 1 and part.isalpha()):
-            return part.upper()
-
-    newstr = ''.join((ch if ch in '0123456789' else ' ') for ch in file_name)
-    num_arr = newstr.split(' ')
-    if(len(num_arr) == 1):
-        numm = int(num_arr[0])
-        if(numm < 26):
-            return chr(ord('A') + numm - 1)
-
-    return None
-
-if __name__ == "__main__":
-    folder = input('enter folder name : ')
-    print(get_contest_name(folder))
-    file_name = input('enter file name : ')
-    print(get_problem_name(file_name))
-
+    return global_config.get_problem_name(file_name)
