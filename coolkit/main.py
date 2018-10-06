1b384fed0f68ca6a99e95a39a89a0f03f469004e
Sarbjit Singh <srbcheema1@gmail.com>
2018-10-06 05:34:08 +0530

    Added view subparser
    - user to view user
    - upcoming to view upcoming contest
    - contest to view contest
    - problem to view problem

--- coolkit/main.py
+++ coolkit/main.py
@@ -6,6 +6,7 @@ import sys
 
 try:
     import getpass
+    from terminaltables import AsciiTable
 except:
     err = """
     You haven't installed the required dependencies.
@@ -14,9 +15,11 @@ except:
     sys.exit(0)
 
 from .lib.Args import Args
-from .lib.Parser import Parser
 from .lib.Colour import Colour
+from .lib.Contest import Contest
 from .lib.global_config import get_problem_name
+from .lib.Parser import Parser
+from .lib.Profile import Dummy_user
 
 coolkit_help="""usage coolkit [option] [--suboptions [args]]
 
@@ -166,9 +169,9 @@ def main():
             if(not Args.check_init()):
                 print(Colour.RED+'no contest provided, either provide contest using -c or run command from a coolkit repo'+Colour.END)
                 sys.exit(0)
-            else:
-                config_data = Args.fetch_data_from_local_config()
-                args['c_name'] = config_data['c_name'] # can be None
+            config_data = args.fetch_data_from_local_config()
+            args['c_name'] = config_data['c_name'] # can be none
+            if(not args['c_name']):
                 print(Colour.RED+'contest not set, use `coolkit set -c <contest num>`, or provide contest name using -c parameter'+Colour.END)
                 sys.exit(0)
         Args.fetch_contest(args)
@@ -177,6 +180,51 @@ def main():
         if(pars_args.user): args['user'] = pars_args.user
         if(pars_args.pswd): args['pswd'] = pars_args.pswd
         Args.set_global_config(args)
+
+    elif(first_arg == "view"):
+        second_arg = pars_args.second_arg
+
+        if(second_arg == "user"):
+            u_name = pars_args.u_name
+            if(not u_name):
+                u_name = input('Enter your username : ')
+            dummy_user = Dummy_user(u_name,verbose=False)
+            dummy_user.print_data()
+            print(AsciiTable(dummy_user.contest_table).table)
+
+        elif(second_arg == "contest"):
+            c_name = pars_args.c_name
+            if(not c_name):
+                if(Args.check_init()):
+                    print(Colour.YELLOW+'contest not set, use `coolkit set -c <contest num>`'+Colour.END)
+                c_name = input('Enter contest name : ')
+
+            temp_contest = Contest(c_name)
+            temp_contest.pull_contest()
+            temp_contest.display_contest()
+
+        elif(second_arg == "prob"):
+            if(not Args.check_init()):
+                print(Colour.RED+'please run this command from a coolkit repo'+Colour.END)
+                sys.exit(0)
+            p_name = pars_args.p_name
+            c_name = Args.fetch_data_from_local_config()['c_name']
+            if(not c_name):
+                print(Colour.YELLOW+'contest not set, use `coolkit set -c <contest num>`'+Colour.END)
+                c_name = input('Enter contest name : ')
+                Args.set_local_config({'c_name',c_name})
+            if(not p_name):
+                print(Colour.YELLOW+'problem not set, use `coolkit set -p <problem name>`'+Colour.END)
+                p_name = input('Enter contest name : ')
+                Args.set_local_config({'p_name',p_name})
+
+            prob = Problem(p_name,c_name)
+            prob.pull_problem(force=False)
+            prob.display_problem()
+            print(Colour.CYAN+prob.link+Colour.END)
+
+        elif(second_arg == "upcoming"):
+            Contest.upcoming_contest(display=True)
 
 
 if __name__ == '__main__':
