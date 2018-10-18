634f010878d1ee7ea1a376c121e92fb7b1fc9776
abhay-recon <abhayofficialone@gmail.com>
2018-10-18 23:52:07 +0530

    >Significant> 	Added support different package manager based on their availability, 	rather than based on distro in use.
    >Also>
    	Added print_clr() to avoid ambiguities on non-error printed as errors,
    	rather than print_err() which writes on 'stderr', for colour.

--- coolkit/dependencies/dependencies.py
+++ coolkit/dependencies/dependencies.py
@@ -1,5 +1,16 @@
 dependency_map = {
     'register-python-argcomplete':{
-        'ubuntu':'sudo apt install python-argcomplete',
+           'apt':'sudo apt install python-argcomplete',#debian/ubuntu-based
+        'pacman':'sudo pacman -S python-argcomplete',#arch-based
+           'yum':'sudo yum -y install python-argcomplete',#red-hat
+           'dnf':'sudo dnf install python-argcomplete',#fedora
+        'zypper':'sudo zypper install python-argcomplete',#suse
+    },
+    'figlet':{
+           'apt':'sudo apt install figlet',
+        'pacman':'sudo pacman -S figlet',
+           'yum':'sudo yum -y install figlet',
+           'dnf':'sudo dnf install figlet',
+        'zypper':'sudo zypper install figlet',
     },
 }
--- coolkit/dependencies/dependency.py
+++ coolkit/dependencies/dependency.py
@@ -2,22 +2,21 @@ import os
 import subprocess
 import sys
 
+import dependencies
+
 class C: # for colours
     R = '\033[91m'
     G = '\033[92m'
     Y = '\033[93m'
     E = '\033[0m'
 
+
+#~~handlers~~
 def print_err(msg,colour=''):
     sys.stderr.write(colour+msg+'\n'+C.E)
 
-def _get_supported_distros(dependency_map):
-    supported_distros = set()
-    for rules in dependency_map.values():
-        for key in rules.keys():
-            supported_distros.add(key)
-    return supported_distros
-
+def print_clr(msg,colour=''):
+    print(colour+msg+C.E)
 
 def line_adder(filename, line):
     with open(filename, 'r+') as f:
@@ -27,18 +26,7 @@ def line_adder(filename, line):
         if(not line in lines):
             f.seek(0, 0)
             f.write(content + '\n' + line + '\n')
-
-
-def get_distro(distros):
-    try:
-        p = subprocess.Popen(['uname','-a'], stdout=subprocess.PIPE)
-        out = p.stdout.read().decode('utf-8').lower()
-        for d in distros:
-            if d in out:
-                return d
-        return None
-    except:
-        return None
+#--handlers--
 
 
 def is_installed(soft):
@@ -55,11 +43,27 @@ def is_installed(soft):
     return False
 
 
+def get_available_package_manager(dependency_map):
+    package_managers = set()
+    for rules in dependency_map.values():
+        for key in rules.keys():
+            package_managers.add(key)
+
+    try:
+        for package_manager in package_managers :
+            if is_installed(package_manager) :
+                return package_manager
+        return None
+    except:
+        return None
+
+
 def install_arg_complete():
     if is_installed('register-python-argcomplete'):
         line = 'eval "$(register-python-argcomplete coolkit)"'
         filename = os.environ['HOME'] + '/.bashrc'
         line_adder(filename,line)
+
 
 def set_global_config():
     path = abs_path('~/.config/coolkit/global_config.py')
@@ -69,42 +73,47 @@ def set_global_config():
     verify_file('~/.config/coolkit/global_config.py')
     shutil.copy(path_of_default_global_config, path)
 
+
 def install_dependencies(dependency_map, verbose = False):
-    supported_distros = _get_supported_distros(dependency_map)
-    distro = get_distro(supported_distros)
-    if(not distro and verbose):
-        print_err('unrecognised distro, please contact srbcheema2@gmail.com for full support for your distro',C.R)
-    elif(distro and verbose):
-        print_err('Distro detected to be '+distro+' based',C.G)
-
     all_installed = True
+    package_manager = get_available_package_manager(dependency_map)
 
-    for d in dependency_map.keys():
-        if is_installed(d):
-            continue
-        rules = dependency_map[d]
-        if distro and distro in rules.keys():
-            print_err('installing '+d+' dependency',C.G)
-            os.system(rules[distro])
-            if not is_installed(d):
-                print_err('please install ' +d+ ' dependency manually',C.Y)
-                print_err('try command : '+rules[distro],C.Y)
+#--handling-absence-of-package-manager--
+    if not package_manager :
+        all_installed = False
+        if verbose :
+            print_err('Error: Unrecognised package manager',C.R)
+            print_err('Please contact srbcheema2@gmail.com for full support for your system.')
+
+            for dependency in dependency_map.keys() :
+                if not is_installed(dependency) :
+                    print_err('Please install ' + dependency + ' dependency manually',C.R)
+#--absence-of-package-manager-handled-
+
+    else :
+        if verbose :
+            print_clr('Package manager detected to be '+ C.E + package_manager , C.G) #distinction of package manager
+
+        for dependency in dependency_map.keys():
+
+            if is_installed(dependency):
+                if verbose :
+                    print_clr('.:Dependency '+dependency+' already installed...',C.G)
+                continue
+
+            rules = dependency_map[dependency]
+
+            print_clr('.:Installing ' + C.E + dependency + C.G +' dependency' , C.G) #distinction of dependency
+            os.system(rules[package_manager])
+
+            if not is_installed(dependency):
+                print_err('please install ' + dependency + ' dependency manually',C.Y)
+                print_err('try command : '+rules[package_manager],C.Y)
                 all_installed = False
-        else:
-            print_err('Please install ' +d+ ' dependency manually',C.R)
-            all_installed = False
 
     return all_installed
 
 
 if __name__ == '__main__':
-    dependency_map = {
-        'register-python-argcomplete':{
-            'ubuntu':'sudo apt install python-argcomplete',
-        },
-        'figlet':{
-            'ubuntu':'sudo apt install figlet',
-        },
-    }
-    install_dependencies(dependency_map,verbose=True)
+    install_dependencies(dependencies.dependency_map,verbose=True)
     install_arg_complete()
